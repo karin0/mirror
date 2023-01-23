4865604cffc2ec80cb20730b9ac8e10d16974a36
karin0 <karin0@gmx.com>
2023-01-23 23:08:46 +0800

    allow forced updates from remote

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use serde::Deserialize;
 use std::collections::HashMap;
 use std::ops::Deref;
 use std::sync::{Arc, Mutex};
-use std::thread;
 use std::time::Duration;
+use std::{fmt, thread};
 use tiny_http::{Response, Server};
 
 type Result<T> = std::result::Result<T, Error>;
@@ -26,7 +26,7 @@ impl<'a> Repo<'a> {
     }
 
     fn proxy_opts(&self) -> Option<ProxyOptions> {
-        self.proxy.as_ref().map(|p| {
+        self.proxy.map(|p| {
             let mut r = ProxyOptions::new();
             r.url(p);
             r
@@ -68,9 +68,8 @@ impl<'a> Repo<'a> {
         }
         let mut res = HashMap::with_capacity(specs.len());
         for spec in specs {
-            let specs: &[&str] = &[&spec];
             let mut opts = self.fetch_opts();
-            remote.fetch(specs, opts.as_mut(), None)?;
+            remote.fetch(&[&spec], opts.as_mut(), None)?;
             match self.repo.find_reference("FETCH_HEAD") {
                 Ok(r) => {
                     let oid = r.target().unwrap();
@@ -90,23 +89,25 @@ impl<'a> Repo<'a> {
     }
 }
 
-fn merge(repo: &Repository, branch: &str, oid: Option<Oid>) -> Result<()> {
+fn merge(repo: &Repository, branch: &str, oid: Option<Oid>) -> Result<Option<Oid>> {
     let ref_name = format!("refs/heads/{}", branch);
     match repo.find_reference(&ref_name) {
         Ok(mut r) => {
+            let old = r.target();
             if let Some(oid) = oid {
                 r.set_target(oid, "mirror update")?;
             } else {
                 r.delete()?;
             }
+            Ok(old)
         }
         Err(_) => {
             if let Some(oid) = oid {
                 repo.reference(&ref_name, oid, false, "mirror new")?;
             }
+            Ok(None)
         }
-    };
-    Ok(())
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -133,11 +134,61 @@ fn stat_remote<S: AsRef<str>, K: Iterator<Item = S>>(
     res
 }
 
-fn work(path: &str, a_url: &str, b_url: &str, proxy: Option<&str>) -> Result<()> {
+struct NewRef(Option<Oid>);
+
+impl fmt::Display for NewRef {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.0 {
+            Some(oid) => write!(f, "{:.7}", oid),
+            None => write!(f, "<deleted>"),
+        }
+    }
+}
+
+impl From<Option<Oid>> for NewRef {
+    fn from(oid: Option<Oid>) -> Self {
+        NewRef(oid)
+    }
+}
+
+struct SavedRefs<'a> {
+    refs: Vec<(String, Option<Oid>)>,
+    repo: &'a Repository,
+}
+
+impl<'a> SavedRefs<'a> {
+    fn new(repo: &'a Repository) -> Self {
+        Self {
+            refs: Vec::new(),
+            repo,
+        }
+    }
+
+    fn save(&mut self, br: String, oid: Option<Oid>) {
+        self.refs.push((br, oid));
+    }
+
+    fn close(mut self) {
+        self.refs.clear();
+    }
+}
+
+impl Drop for SavedRefs<'_> {
+    fn drop(&mut self) {
+        for (br, oid) in &self.refs {
+            if let Err(e) = merge(self.repo, br, *oid) {
+                eprintln!("restore {} to {:?}: {:?}", br, oid, e);
+            }
+        }
+    }
+}
+
+fn work(path: &str, a_url: &str, b_url: &str, proxy: Option<&str>, debug: bool) -> Result<()> {
     let repo = Repository::init(path)?;
     let ctx = Repo { repo: &repo, proxy };
     let mut remote_a = ctx.set_remote("a", a_url)?;
     let mut a = ctx.fetch(&mut remote_a)?;
+    let a_raw = a.clone();
 
     let branches = repo.branches(Some(BranchType::Local))?;
     let mut status = HashMap::with_capacity(branches.size_hint().0);
@@ -161,6 +212,7 @@ fn work(path: &str, a_url: &str, b_url: &str, proxy: Option<&str>) -> Result<()>
 
     let mut remote_b = ctx.set_remote("b", b_url)?;
     let b = ctx.fetch(&mut remote_b)?;
+    let b_raw = b.clone();
 
     let mut cnt_a = 0;
     let mut cnt_b = 0;
@@ -187,17 +239,17 @@ fn work(path: &str, a_url: &str, b_url: &str, proxy: Option<&str>) -> Result<()>
                     } else if let Some(a_now) = *now {
                         if let Some(b_now) = b_now {
                             if repo.graph_descendant_of(a_now, b_now)? {
-                                eprintln!("{}: {}: ff to {} (A)", path, br, a_now);
+                                eprintln!("{}@{}:{} ff to A", path, br, a_now);
                                 cnt_a += 1;
                             } else if repo.graph_descendant_of(b_now, a_now)? {
-                                eprintln!("{}: {}: ff to {} (B)", path, br, b_now);
+                                eprintln!("{}@{}:{} ff to B", path, br, b_now);
                                 *now = Some(b_now);
                                 *st = RefUpdate::B;
                                 cnt_b += 1;
                             } else {
                                 if let Ok((ahead, behind)) = repo.graph_ahead_behind(b_now, a_now) {
                                     eprintln!(
-                                        "{}: {}: {} ahead, {} behind",
+                                        "{}@{}: {} ahead, {} behind",
                                         path, br, ahead, behind
                                     );
                                 }
@@ -218,69 +270,69 @@ fn work(path: &str, a_url: &str, b_url: &str, proxy: Option<&str>) -> Result<()>
             cnt_b += 1;
         }
     }
-
-    if !changed {
-        // eprintln!(
-        //     "{}: {} branches unchanged: {}",
-        //     path,
-        //     status.len(),
-        //     status
-        //         .keys()
-        //         .into_iter()
-        //         .map(|x| x.as_ref())
-        //         .collect::<Vec<&str>>()
-        //         .join(", ")
-        // );
+    if !debug && !changed {
         return Ok(());
     }
 
     let mut to_a = Vec::with_capacity(cnt_b);
     let mut to_b = Vec::with_capacity(cnt_a);
+    let mut old_refs = SavedRefs::new(&repo);
     for (br, (st, now)) in status {
-        let delete = now.is_none();
-        let push_to = |v: &mut Vec<String>| {
-            let spec = if delete {
-                format!(":refs/heads/{}", br)
-            } else {
-                format!("refs/heads/{}:refs/heads/{}", br, br)
-            };
-            v.push(spec);
+        let spec_to = |remote: &HashMap<String, Oid>| -> Result<String> {
+            Ok(match now {
+                None => format!(":refs/heads/{}", br),
+                Some(now) => {
+                    if let Some(old) = remote.get(&br) {
+                        if !repo.graph_descendant_of(now, *old)? {
+                            eprintln!("{}@{}:{} forced update from {}", path, br, now, old);
+                            return Ok(format!("+refs/heads/{}:refs/heads/{}", br, br));
+                        }
+                    }
+                    format!("refs/heads/{}:refs/heads/{}", br, br)
+                }
+            })
         };
-        let now_ref = || match &now {
-            Some(oid) => oid.to_string(),
-            None => "<deleted>".to_string(),
-        };
+        let new = NewRef::from(now);
         match st {
             RefUpdate::Unchanged => {
-                eprintln!("{}: {}:{} unchanged", path, br, now_ref());
+                eprintln!("{}@{}:{} unchanged", path, br, new);
+                continue;
             }
             RefUpdate::A(_) => {
-                merge(&repo, &br, now)?;
-                eprintln!("{}: {}:{} -> B", path, br, now_ref());
-                push_to(&mut to_b);
+                eprintln!("{}@{}:{} -> B", path, br, new);
+                to_b.push(spec_to(&b_raw)?);
             }
             RefUpdate::B => {
-                merge(&repo, &br, now)?;
-                eprintln!("{}: {}:{} -> A", path, br, now_ref());
-                push_to(&mut to_a);
+                eprintln!("{}@{}:{} -> A", path, br, new);
+                to_a.push(spec_to(&a_raw)?);
             }
             RefUpdate::Both => {
-                eprintln!("{}: {}:{} both changed", path, br, now_ref());
-                merge(&repo, &br, now)?;
+                eprintln!("{}@{}:{} both changed", path, br, new);
             }
             RefUpdate::Conflict => {
-                eprintln!("{}: {}: conflict!", path, br);
+                eprintln!("{}@{}: conflict!", path, br);
+                continue;
             }
-        }
+        };
+        let old = merge(&repo, &br, now)?;
+        old_refs.save(br, old);
     }
-
+    assert_eq!(to_b.len(), cnt_a);
+    assert_eq!(to_a.len(), cnt_b);
     let mut opts = ctx.push_opts();
-    if !to_a.is_empty() {
-        remote_a.push(&to_a, opts.as_mut())?;
-    }
-    if !to_b.is_empty() {
-        remote_b.push(&to_b, opts.as_mut())?;
-    }
+    let mut push =
+        |specs: Vec<String>, mut remote: Remote, raw: HashMap<String, Oid>| -> Result<()> {
+            if !specs.is_empty() {
+                if ctx.fetch(&mut remote)? != raw {
+                    return Err(Error::from_str("remote changed"));
+                }
+                remote.push(&specs, opts.as_mut())?;
+            }
+            Ok(())
+        };
+    push(to_a, remote_a, a_raw)?;
+    push(to_b, remote_b, b_raw)?;
+    old_refs.close();
     Ok(())
 }
 
@@ -290,6 +342,7 @@ struct Conf {
     proxy: Option<String>,
     hosts: Option<HashMap<String, String>>,
     bind: Option<String>,
+    debug: Option<bool>,
 }
 
 impl Conf {
@@ -321,6 +374,7 @@ fn main() -> AnyResult<()> {
         parse_url(url_b);
     }
     drop(hosts);
+    let debug = conf.debug.unwrap_or(false);
 
     let arc = Arc::new(Mutex::new((repos, conf.proxy)));
     let app = arc.clone();
@@ -328,7 +382,7 @@ fn main() -> AnyResult<()> {
         let app = app.lock().unwrap();
         let (repos, proxy) = app.deref();
         for (path, (url_a, url_b)) in repos {
-            if let Err(e) = work(path, url_a, url_b, proxy.as_deref()) {
+            if let Err(e) = work(path, url_a, url_b, proxy.as_deref(), debug) {
                 eprintln!("{}: {}", path, e);
             }
         }
@@ -346,11 +400,16 @@ fn main() -> AnyResult<()> {
         for request in server.incoming_requests() {
             let method = request.method();
             let url = request.url();
+            if debug {
+                for header in request.headers() {
+                    eprintln!("{} {}: {:?}", method, url, header);
+                }
+            }
             let path = &url[1..];
             let app = app.lock().unwrap();
             let (repos, proxy) = app.deref();
             if let Some((url_a, url_b)) = repos.get(path) {
-                if let Err(e) = work(path, url_a, url_b, proxy.as_deref()) {
+                if let Err(e) = work(path, url_a, url_b, proxy.as_deref(), debug) {
                     eprintln!("{} {}: {}", method, url, e);
                 } else {
                     eprintln!("{} {}: ok", method, url);
